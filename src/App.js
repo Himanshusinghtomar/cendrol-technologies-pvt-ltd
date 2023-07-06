f12c9685c5d75457e3d387202370986c83efac33
Himanshu Singh Tomar <himanshusinghtomar204@gmail.com>
2023-07-06 23:54:35 +0530

    change on bulid test

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   useEffect(() => {
     document.body.style.backgroundImage = `linear-gradient(265deg, rgba(125,166,251,1) 22%, rgba(0,0,0,1) 89%)`
   })
+  
   return (
     <div className="mt-3">
       <h1 className=' text-5xl text-center text-green-600 animate-bounce font-extrabold'>Chuck Norries</h1>
